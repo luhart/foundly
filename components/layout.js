c508a1dcdfcb3e486fcef1c40a3fb65f5768c717
Luke Hartman <ljhartman10@gmail.com>
2020-11-03 15:34:03 -0800

    new ico

--- components/layout.js
+++ components/layout.js
@@ -14,10 +14,7 @@ export default function Layout({ children, home }) {
   return (
     <>
       <Head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.ico" />
         <meta
           name="description"
           content="become a company"
